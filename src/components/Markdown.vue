61b98aa78d678a9a5f0e97f3fd617f494e94a55a
liu2ye <liu2ye@outlook.com>
2024-10-16 02:57:11 +0800

    修改uno配置，扫描js文件

--- src/components/Markdown.vue
+++ src/components/Markdown.vue
@@ -17,11 +17,6 @@ if (props.fileName) {
 </script>
 <template>
   <div class="markdown-body" v-html="marked.parse(text)"></div>
-  <!-- 防止unocss不打包marked使用的样式 -->
-  <div
-    style="display: none"
-    class="hidden cursor-pointer flex items-center text-white bg-black/30 absolute top-0 right-0 uppercase font-bold text-xs rounded-bl-md px-2 py-1 bg-teal-950 shadow-xl text-sm relative overflow-hidden max-w-full rounded p-4 text-gray-200"
-  ></div>
 </template>
 <style lang="scss">
 pre {
--- uno.config.js
+++ uno.config.js
@@ -2,5 +2,15 @@
 import { defineConfig, presetUno, presetAttributify, presetIcons } from 'unocss'
 
 export default defineConfig({
-  presets: [presetUno(), presetAttributify(), presetIcons()]
+  presets: [presetUno(), presetAttributify(), presetIcons()],
+  content: {
+    pipeline: {
+      include: [
+        // 默认
+        /\.(vue|svelte|[jt]sx|mdx?|astro|elm|php|phtml|html)($|\?)/,
+        // 包括 js 文件
+        'src/**/*.js'
+      ]
+    }
+  }
 })
